aa99c0339e95748c964cba5d3aacfdbc6b528aaf
Masooma <masooma.1022@gmail.com>
2026-04-09 19:44:38 +0300

    Updated

--- index.js
+++ index.js
@@ -2,35 +2,49 @@
  * Selects the element with the id 'main-heading' and changes its text content to 'DOM Manipulation Challenge'.
  */
 function changeHeadingText() {
-  // TODO: Implement this function
+  const heading = document.getElementById('main-haeding');
+  if(heading){
+    heading.textContent = 'DOM Manipulation Challenge'
+  }
 }
 
 /**
  * Selects the element with the id 'box-to-modify' and changes its background color to 'lightblue'.
  */
 function changeBoxColor() {
-  // TODO: Implement this function
+  const box = document.getElementById('box-to-modify');
+  if(box){
+    box.style.backgroundColor = 'lightblue';
+  }
 }
 
 /**
  * Creates a new <li> element, sets its text content to 'New Item', and appends it to the <ul> with the id 'item-list'.
  */
 function addNewItem() {
-  // TODO: Implement this function
+  const list = document.getElementById('item-list');
+  newIteam.textContent = 'New Item';
+  list.appendChild(newIteam);
 }
 
 /**
  * Selects the paragraph with the class 'content-para' and adds the class 'highlight' to it.
  */
 function highlightParagraph() {
-  // TODO: Implement this function
+  const para =  document.querySelector('.content-para');
+  if(para){
+    para.classList.add('highlight');
+  }
 }
 
 /**
  * Selects the element with the id 'to-be-removed' and removes it from the DOM.
  */
 function removeElement() {
-  // TODO: Implement this function
+  const element = document.GetElemetById('to-be-removed');
+  if(element){
+    element.remove();
+  }
 }
 
 
